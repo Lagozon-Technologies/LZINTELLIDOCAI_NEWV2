17026c0c6cf9e14d608eec7d62412e691daf5c2c
arunaagrawal <168619789+arunaagrawal@users.noreply.github.com>
2024-09-23 15:55:50 +0800

    Update for pysqlite3
    Update for pysqlite3

--- new_main.py
+++ new_main.py
@@ -20,9 +20,9 @@
 # ********************************************************************************************** #
 
 #Added by Aruna for chromaDB SQLite version error
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+#__import__('pysqlite3')
+#import sys
+#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 import os
